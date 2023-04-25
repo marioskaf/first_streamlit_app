606ac1368b237d41b2796a8d33aa5332a2197335
Mario Skaf <mario.skaf@snowflake.com>
2023-04-24 19:41:38 -0700

    Update streamlit_app.py
    added more details to the page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
+
+srreamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamiit.text('Hard-Boiled Free-Range Egg')
