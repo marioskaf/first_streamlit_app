502a6c363f00be1bbc8f2e8f1370209e02f36d6f
Mario Skaf <mario.skaf@snowflake.com>
2023-04-27 19:52:27 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,37 +27,21 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 
+
+streamlit.header('Fruityvice Fruit Advice!')
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
-else:
-    try:
+    else:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_response.raise_for_status()  # Raise an exception if the response contains an HTTP error
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
         streamlit.dataframe(fruityvice_normalized)
+        
 except URLError as e:
-streamlit.stop()
+    streamlit.error()
 
-
-
-
-
-
-
-
-
-
-#streamlit.header('Fruityvice Fruit Advice!')
-#try:
- # fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-#if not fruit_choice:
- # streamlit.error("Please select a fruit to get information.")
-#else:
-   # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-   # fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    #streamlit.dataframe(fruityvice_normalized)
-
-
+stremlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
